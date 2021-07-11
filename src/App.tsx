7469bc30289286865f13223d8906fcb82c2a6a72
silasbispo01 <silasbispo01@gmail.com>
2021-07-11 12:36:04 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+
+import './styles/global.scss';
+
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar handleClickButton={handleClickButton} selectedGenreId={selectedGenreId}/>
+      <Content selectedGenreId={selectedGenreId}/>
+    </div>
+      
+  )
+}
